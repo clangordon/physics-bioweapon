4b7ba69c0488c695bb4d8c50bac4443fba59d718
Andrew Potter <hello@drewpotter.com>
2024-09-24 15:22:31 +0100

    print

--- physicsbioweapon.py
+++ physicsbioweapon.py
@@ -4,5 +4,8 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 #
+import os
 
 print("Clan Gordon Physics Bioweapon")
+print("Licensed under the AGPLv3")
+print("Secret ENV variables are commercially available on request")
