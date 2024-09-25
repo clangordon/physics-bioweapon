0ae7e4f7f655050bc379d56a584b0456d4c810a0
Andrew Potter <hello@drewpotter.com>
2024-09-25 15:06:17 +0100

    env vars

--- physicsbioweapon.py
+++ physicsbioweapon.py
@@ -9,3 +9,12 @@ import os
 print("Clan Gordon Physics Bioweapon")
 print("Licensed under the AGPLv3")
 print("Secret ENV variables are commercially available on request")
+
+cardiac_arrest = os.environ.get("PHYSICS_BIOWEAPON_CARDIAC_ARREST")
+print(cardiac_arrest)
+
+water_resonant_freq = os.environ.get("PHYSICS_BIOWEAPON_WATER_RESONANT_FREQ")
+print(water_resonant_freq)
+
+bioacoustic_resonant_freqs = os.environ.get("PHYSICS_BIOWEAPON_BIOACOUSTIC_RESONANT_FREQS")
+print(bioacoustic_resonant_freqs)
